12cc7ed4e8bad7316a563593676be0136c2358d9
Gabriel-Munoz <g.munoz1@ufl.edu>
2024-04-18 17:59:20 -0400

    fix definitions etc

--- Heap_structure.cpp
+++ Heap_structure.cpp
@@ -2,11 +2,11 @@
 // Created by gabri on 4/16/2024.
 //
 
-#include "Heap_structure.h"
+//#include "Heap_structure.h"
 #include <vector>
 using namespace std;
 template<typename T>
-void heapify_up(vector<T>& vec, T attribute){
+inline void heapify_up(vector<T>& vec, T attribute){
 vec.push_back(attribute);
 int index = vec.size() - 1;
         while(vec[index] < vec[(index - 1)/2] && index != 0){
@@ -17,7 +17,7 @@ int index = vec.size() - 1;
     }
 }
 template<typename T>
-void heapify_down(vector<T>& vec){
+inline void heapify_down(vector<T>& vec){
     int i = 0;
     while((2*i)+1 < vec.size() && (vec[i] > vec[(2*i)+1] || (vec[i] > vec[(2*i)+2]))){
         if(vec[(2*i)+1] < vec[(2*i)+2]){
