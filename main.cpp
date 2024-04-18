d4e795f1f3a5143fdb4a13704440b7e37362eb6a
AlejandroMate0507 <alejandro.mateus@ufl.edu>
2024-04-18 15:55:44 -0400

    implemented the try again loop for inserting a song name

--- main.cpp
+++ main.cpp
@@ -15,75 +15,72 @@ int main() {
     string option; // Heap sort or Quick sort
     int playlistSize;
     int attribute; // Holds the comparison attribute
+    bool songFound = false; // exits loop when the song is found
 
     // Taking user input
     cout << "Please enter a song name\n-";
     cin >> songName;
 
-    //parsing data
-    string filename = "../dataset.csv";
-    ifstream file(filename);
+    while(!songFound){
+        //parsing data
+        string filename = "../dataset.csv";
+        ifstream file(filename);
 
-    if (!file.is_open()) {
-        cerr << "Error opening file: " << filename << endl;
-    }
+        if (!file.is_open()) {
+            cerr << "Error opening file: " << filename << endl;
+        }
 
-    string space;
+        string space;
 
-    string line;
-    getline(file, line); // to ignore the first line with the column titles
-    for (int i = 0; i < 131; i++) {
-        getline(file, line);
-        stringstream ss(line);
+        string line;
+        getline(file, line); // to ignore the first line with the column titles
+        for (int i = 0; i < 131; i++) {
+            getline(file, line);
+            stringstream ss(line);
 
-        vector<string> tokens;
-        string token;
-        bool inQuotes = false;
-        string field;
+            vector<string> tokens;
+            string token;
+            bool inQuotes = false;
+            string field;
 
-        while (getline(ss, token, ',')) {
-            if (inQuotes == false) {
-                if (token.front() == '"' && token.back() != '"') {
-                    inQuotes = true;
-                    field = token + ",";
+            while (getline(ss, token, ',')) {
+                if (inQuotes == false) {
+                    if (token.front() == '"' && token.back() != '"') {
+                        inQuotes = true;
+                        field = token + ",";
+                    }
+                    else {
+                        tokens.push_back(token);
+                    }
                 }
                 else {
-                    tokens.push_back(token);
+                    field += token;
+                    if (token.back() == '"') {
+                        inQuotes = false;
+                        tokens.push_back(field);
+                    }
+                    else {
+                        field += ",";
+                    }
                 }
             }
-            else {
-                field += token;
-                if (token.back() == '"') {
-                    inQuotes = false;
-                    tokens.push_back(field);
-                }
-                else {
-                    field += ",";
-                }
+            if (songName == tokens[4]) {
+                cout << tokens[0] << endl;
+                songFound = true;
             }
         }
-//        for (int k = 0; k < tokens.size(); k++) {
-//            cout << k << ": " << tokens[k] << endl;
-//        }
-        if (songName == tokens[4]) {
-            cout << tokens[0] << endl;
+        file.close();
+        if(!songFound){
+            cout << "This song was not found. Please enter another song name.\n-";
+            cin >> songName;
         }
     }
-    file.close();
-
-
-
 
     cout << "Please choose the attribute for comparison\n"
             "   1. Popularity\n"
             "   2. Danceability\n"
             "   3. Energy\n"
-            "   4. Loudness\n"
-            "   5. Speechiness\n"
-            "   6. Acousticness\n"
-            "   7. Liveness\n"
-            "   8. Valence\n"
-            "   9. Tempo\n-";
+            "   4. Loudness\n-";
     cin >> attribute;
     cout << "Would you like to use Heap sort or Quick sort (\"H\" for Heap/anything else for Quick)\n-";
     cin >> option;
